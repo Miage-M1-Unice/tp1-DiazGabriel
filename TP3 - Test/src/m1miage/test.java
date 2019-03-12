ae44738d77088fc245988db22a342911f3143980
DiazGabriel <diaz.gabriel@orange.fr>
2019-03-12 10:49:44 +0100

    TP3 : URLClassLoader

---
+++ TP3 - Test/src/m1miage/test.java
@@ -0,0 +1,11 @@
+package m1miage;
+
+public class test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("classe test");
+			
+	}
+
+}
---
+++ TP3/src/m1miage/URLCLassLoader.java
@@ -0,0 +1,13 @@
+package m1miage;
+
+import java.lang.ClassLoader;
+
+public class URLCLassLoader {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+	
+}

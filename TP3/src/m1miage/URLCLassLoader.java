48773181b5a33c402d4f780ed3d6b88a263db3a1
DiazGabriel <diaz.gabriel@orange.fr>
2019-03-12 23:06:30 +0100

    TP3 : urlClassLoader

--- TP3/src/m1miage/URLCLassLoader.java
+++ TP3/src/m1miage/URLCLassLoader.java
@@ -1,13 +1,22 @@
 package m1miage;
 
-import java.lang.ClassLoader;
+import java.net.*;
 
 public class URLCLassLoader {
 
 	public static void main(String[] args) {
-		
-		
-		
-	}
+	  try {
+	  	  URL[] urlFile= new URL[]{new URL("file:///C:/Users/diazg/eclipse-workspace/TP3 - Test/")};
+	      URLClassLoader urlLoader ;
+          urlLoader = new URLClassLoader(urlFile);
+          urlLoader.loadClass("m1miage.test");
+      } catch (MalformedURLException e) {
+          e.printStackTrace();
+      } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+	  }
+    }
 	
 }
+	
+
